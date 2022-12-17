b2bee98580db303c44b3723ad235fdbf2b4540f2
Eben Freeman <Eben.Freeman@ayahealthcare.com>
2022-12-16 16:41:46 -0800

    Background service and test

---
+++ Package/Package.Infrastructure.BackgroundService/BackgroundServices.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Collections.Concurrent;
+
+namespace Infrastructure.BackgroundServices;
+
+//https://blog.elmah.io/async-processing-of-long-running-tasks-in-asp-net-core/amp/
+
+public class BackgroundTaskQueue : IBackgroundTaskQueue
+{
+    private readonly ConcurrentQueue<Func<CancellationToken, Task>> _workItems = new();
+    private readonly SemaphoreSlim _signal = new(0);
+
+    public async Task<Func<CancellationToken, Task>?> DequeueAsync(CancellationToken cancellationToken)
+    {
+        await _signal.WaitAsync(cancellationToken);
+        _workItems.TryDequeue(out var workItem);
+        return workItem;
+    }
+
+    public void QueueBackgroundWorkItem(Func<CancellationToken, Task> workItem)
+    {
+        _ = workItem ?? throw new ArgumentNullException(nameof(workItem));
+
+        _workItems.Enqueue(workItem);
+        _signal.Release();
+    }
+}
+
+/// <summary>
+/// Fire-And-Forget / long running background task
+/// </summary>
+public class BackgroundTaskService : BackgroundService
+{
+    private readonly IBackgroundTaskQueue _taskQueue;
+    private readonly ILogger<BackgroundTaskService> _logger;
+
+    public BackgroundTaskService(IBackgroundTaskQueue taskQueue, ILogger<BackgroundTaskService> logger)
+    {
+        _taskQueue = taskQueue;
+        _logger = logger;
+    }
+
+    protected async override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        //keep checking the queue
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            var workItem = await _taskQueue.DequeueAsync(stoppingToken);
+            if (workItem != null)
+            {
+                try
+                {
+                    _ = workItem(stoppingToken);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "ExecuteAsync Fail");
+                }
+            }
+        }
+    }
+
+    public override async Task StopAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("BackgroundTaskService Hosted Service is stopping.");
+
+        await base.StopAsync(cancellationToken);
+    }
+}
+
---
+++ Package/Package.Infrastructure.Test.Integration/IntegrationTestBase.cs
@@ -0,0 +1,33 @@
+﻿using Infrastructure.BackgroundServices;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Test.Integration;
+
+[TestClass]
+public abstract class IntegrationTestBase
+{
+    protected readonly IServiceProvider Services;
+    protected readonly ILoggerFactory LoggerFactory;
+
+    //[AssemblyInitialize]
+    //public static void Initialize(TestContext ctx) //ctx required for [AssemblyInitialize] run 
+    protected IntegrationTestBase()
+    {
+        //DI
+        ServiceCollection services = new();
+
+        //queued background service - fire and forget 
+        services.AddHostedService<BackgroundTaskService>();
+        services.AddSingleton<IBackgroundTaskQueue, BackgroundTaskQueue>();
+
+        services.AddLogging(configure => configure.AddConsole().AddDebug());
+
+        //build IServiceProvider for subsequent use finding/injecting services
+        Services = services.BuildServiceProvider();
+
+        LoggerFactory = Services.GetRequiredService<ILoggerFactory>();
+    }
+
+}
