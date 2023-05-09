e8d055a4fb858af23b56569a56998a2b1942acbb
Eben Freeman <eben.freeman@adintuitive.com>
2023-05-09 19:26:31 -0400

    file scope namespace

--- Infrastructure.Data/Migrations/20230505203415_InitialCreate.cs
+++ Infrastructure.Data/Migrations/20230505203415_InitialCreate.cs
@@ -3,52 +3,51 @@ using System;
 
 #nullable disable
 
-namespace Infrastructure.Data.Migrations
+namespace Infrastructure.Data.Migrations;
+
+/// <inheritdoc />
+public partial class InitialCreate : Migration
 {
     /// <inheritdoc />
-    public partial class InitialCreate : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.EnsureSchema(
-                name: "todo");
+        migrationBuilder.EnsureSchema(
+            name: "todo");
 
-            migrationBuilder.CreateTable(
-                name: "TodoItem",
-                schema: "todo",
-                columns: table => new
-                {
-                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
-                    Status = table.Column<int>(type: "int", nullable: false),
-                    RowVersion = table.Column<byte[]>(type: "rowversion", rowVersion: true, nullable: false),
-                    CreatedDate = table.Column<DateTime>(type: "datetime2(0)", nullable: false),
-                    CreatedBy = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
-                    UpdatedDate = table.Column<DateTime>(type: "datetime2(0)", nullable: false),
-                    UpdatedBy = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_TodoItem", x => x.Id)
-                        .Annotation("SqlServer:Clustered", false);
-                });
+        migrationBuilder.CreateTable(
+            name: "TodoItem",
+            schema: "todo",
+            columns: table => new
+            {
+                Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                Status = table.Column<int>(type: "int", nullable: false),
+                RowVersion = table.Column<byte[]>(type: "rowversion", rowVersion: true, nullable: false),
+                CreatedDate = table.Column<DateTime>(type: "datetime2(0)", nullable: false),
+                CreatedBy = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                UpdatedDate = table.Column<DateTime>(type: "datetime2(0)", nullable: false),
+                UpdatedBy = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_TodoItem", x => x.Id)
+                    .Annotation("SqlServer:Clustered", false);
+            });
 
-            migrationBuilder.CreateIndex(
-                name: "IX_TodoItem_Name",
-                schema: "todo",
-                table: "TodoItem",
-                column: "Name",
-                unique: true)
-                .Annotation("SqlServer:Clustered", true);
-        }
+        migrationBuilder.CreateIndex(
+            name: "IX_TodoItem_Name",
+            schema: "todo",
+            table: "TodoItem",
+            column: "Name",
+            unique: true)
+            .Annotation("SqlServer:Clustered", true);
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropTable(
-                name: "TodoItem",
-                schema: "todo");
-        }
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropTable(
+            name: "TodoItem",
+            schema: "todo");
     }
 }
