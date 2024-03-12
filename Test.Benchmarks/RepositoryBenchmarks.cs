d92c73471e7172c34753661bcacf4700cbf88354
Eben Freeman <eben.freeman@adintuitive.com>
2024-03-12 13:20:29 -0400

    Test.Benchmarks

--- Test.Benchmarks/RepositoryBenchmarks.cs
+++ Test.Benchmarks/RepositoryBenchmarks.cs
@@ -6,7 +6,6 @@ using Domain.Model;
 using Infrastructure.Repositories;
 using Microsoft.Extensions.DependencyInjection;
 using Package.Infrastructure.Data.Contracts;
-using SampleApp.Bootstrapper.Automapper;
 using Test.Support;
 
 //https://github.com/dotnet/BenchmarkDotNet
@@ -18,28 +17,12 @@ namespace Test.Benchmarks;
 [RankColumn]
 public class RepositoryBenchmarks : DbIntegrationTestBase
 {
-    private readonly IServiceScope _serviceScope = null!;
-    private readonly ITodoRepositoryQuery _repo = null!;
-
-    public RepositoryBenchmarks()
-    {
-        StartContainerAsync().GetAwaiter().GetResult();
-        ConfigureTestInstanceAsync().GetAwaiter().GetResult();
-
-        _ = ConfigureAutomapper.CreateMapper(
-            [
-                new MappingProfile(),  //map domain <-> app 
-                                       //new GrpcMappingProfile() // map grpc <-> app 
-            ]);
-
-        _serviceScope = Services.CreateScope(); //needed for injecting scoped services
-        _repo = (TodoRepositoryQuery)_serviceScope.ServiceProvider.GetRequiredService(typeof(ITodoRepositoryQuery));
-    }
+    private IServiceScope _serviceScope = null!;
+    private ITodoRepositoryQuery _repo = null!;
 
     [Benchmark]
     public async Task Repo_SearchTodoItemAsync()
     {
-
         var search = new SearchRequest<TodoItemSearchFilter> { PageSize = 10, PageIndex = 1 };
         _ = await _repo.SearchTodoItemAsync(search);
     }
@@ -52,10 +35,20 @@ public class RepositoryBenchmarks : DbIntegrationTestBase
 
     //BenchmarkDotNet does not support async setup/teardown
     ////https://github.com/dotnet/BenchmarkDotNet/issues/1738#issuecomment-1687832731
+    
     [IterationSetup]
     public static void IterationSetup()
     {
         ResetDatabaseAsync().GetAwaiter().GetResult();
+    }
+
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        StartContainerAsync().GetAwaiter().GetResult();
+        ConfigureTestInstanceAsync().GetAwaiter().GetResult();
+        _serviceScope = Services.CreateScope(); //needed for injecting scoped services
+        _repo = (TodoRepositoryQuery)_serviceScope.ServiceProvider.GetRequiredService(typeof(ITodoRepositoryQuery));
     }
 
     [GlobalCleanup]
--- Test.Benchmarks/ValidatorBenchmarks.cs
+++ Test.Benchmarks/ValidatorBenchmarks.cs
@@ -2,11 +2,8 @@
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Order;
 using Domain.Shared.Enums;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using Package.Infrastructure.Common;
-using SampleApp.Bootstrapper;
 using Test.Support;
 
 //https://github.com/dotnet/BenchmarkDotNet
@@ -16,29 +13,13 @@ namespace Test.Benchmarks;
 [MemoryDiagnoser]
 [Orderer(SummaryOrderPolicy.FastestToSlowest)]
 [RankColumn]
-public class ValidatorBenchmarks
+public class ValidatorBenchmarks : DbIntegrationTestBase
 {
-    protected readonly static IConfigurationRoot Config = Utility.BuildConfiguration().AddUserSecrets<ValidatorBenchmarks>().Build();
-    private readonly IServiceScope _serviceScope = null!;
-    private readonly IValidationHelper _validationHelper = null!;
-    private readonly IServiceProvider _services = null!;
-
-    protected IServiceProvider Services => _services;
-
-    public ValidatorBenchmarks()
-    {
-        ServiceCollection services = [];
-        //bootstrapper service registrations - infrastructure, domain, application 
-        services
-            .RegisterInfrastructureServices(Config)
-            .RegisterBackgroundServices(Config)
-            .RegisterDomainServices(Config)
-            .RegisterApplicationServices(Config);
-        services.AddLogging(configure => configure.ClearProviders().AddConsole().AddDebug().AddApplicationInsights());
-        _services = services.BuildServiceProvider(validateScopes: true);
-        _serviceScope = _services.CreateScope(); //needed for injecting scoped services
-        _validationHelper = _serviceScope.ServiceProvider.GetRequiredService<IValidationHelper>();
-    }
+    //protected readonly static IConfigurationRoot Config = Utility.BuildConfiguration().AddUserSecrets<ValidatorBenchmarks>().Build();
+    //private IServiceScope _serviceScope = null!;
+    //private IServiceProvider _services = null!;
+    private IServiceScope _serviceScope = null!;
+    private IValidationHelper _validationHelper = null!;
 
     //[Params(5, 10)]
     //public int NameLength { get; set; }
@@ -74,9 +55,44 @@ public class ValidatorBenchmarks
 
     //BenchmarkDotNet does not support async setup/teardown
     //https://github.com/dotnet/BenchmarkDotNet/issues/1738#issuecomment-1687832731
+
+    [IterationSetup]
+    public static void IterationSetup()
+    {
+        ResetDatabaseAsync().GetAwaiter().GetResult();
+    }
+
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        StartContainerAsync().GetAwaiter().GetResult();
+        ConfigureTestInstanceAsync().GetAwaiter().GetResult();
+        _serviceScope = Services.CreateScope(); //needed for injecting scoped services
+        _validationHelper = _serviceScope.ServiceProvider.GetRequiredService<IValidationHelper>();
+    }
+
     [GlobalCleanup]
     public void GlobalCleanup()
     {
         _serviceScope.Dispose();
+        StopContainerAsync().GetAwaiter().GetResult();
     }
+
+    //set up infrastructure not using DbIntegrationTestBase/TestContainer
+    //[GlobalSetup]
+    //public void GlobalSetup()
+    //{
+    //    ServiceCollection services = [];
+    //    //bootstrapper service registrations - infrastructure, domain, application 
+    //    services
+    //        .RegisterInfrastructureServices(Config)
+    //        .RegisterBackgroundServices(Config)
+    //        .RegisterDomainServices(Config)
+    //        .RegisterApplicationServices(Config);
+    //    services.AddLogging(configure => configure.ClearProviders().AddConsole().AddDebug().AddApplicationInsights());
+    //    _serviceScope = Services.CreateScope(); //needed for injecting scoped services
+    //    _validationHelper = _serviceScope.ServiceProvider.GetRequiredService<IValidationHelper>();
+    //}
+
+
 }
