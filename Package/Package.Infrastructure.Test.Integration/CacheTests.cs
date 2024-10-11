4bf6a19a0a270e0da7b562b81bdcf72b29383596
Eben Freeman <eben.freeman@adintuitive.com>
2024-10-11 16:22:01 -0400

    cache tests

--- Package/Package.Infrastructure.Test.Integration/CacheTests.cs
+++ Package/Package.Infrastructure.Test.Integration/CacheTests.cs
@@ -4,7 +4,7 @@ using ZiggyCreatures.Caching.Fusion;
 
 namespace Package.Infrastructure.Test.Integration;
 
-//[Ignore("Redis setup required.")]
+[Ignore("Redis setup required.")]
 
 [TestClass]
 public class CacheTests : IntegrationTestBase
@@ -40,9 +40,21 @@ public class CacheTests : IntegrationTestBase
         cacheItem = await _cache.GetOrSetAsync(key, _ => GetSomeItemAsync());
         Assert.AreEqual(_someDto1.Id, cacheItem?.Id);
 
-        //force refresh - step through to see that cache is not hit, reload with function
+        //force refresh 
         await _cache.RemoveAsync(key);
-        cacheItem = await _cache.GetOrSetAsync(key, _ => GetSomeItemAsync()); 
+        //step through to see that cache is not hit, reload with factory and use custom settings this time
+        var cacheOptions = new FusionCacheEntryOptions()
+        {
+            Duration = TimeSpan.FromMinutes(2),
+            DistributedCacheDuration = TimeSpan.FromMinutes(10),
+            FailSafeMaxDuration = TimeSpan.FromMinutes(10),
+            FailSafeThrottleDuration = TimeSpan.FromSeconds(1),
+            JitterMaxDuration = TimeSpan.FromSeconds(10),
+            FactorySoftTimeout = TimeSpan.FromSeconds(1),
+            FactoryHardTimeout = TimeSpan.FromSeconds(30),
+            EagerRefreshThreshold = 0.9f
+        };
+        cacheItem = await _cache.GetOrSetAsync(key, _ => GetSomeItemAsync(), cacheOptions); 
         Assert.AreEqual(_someDto1.Id, cacheItem?.Id);
 
         //remove from cache
