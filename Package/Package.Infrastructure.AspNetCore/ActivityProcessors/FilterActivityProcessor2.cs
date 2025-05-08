3a54b861112104f0cff74292dd737aeb97044718
Eben Freeman <eben.freeman@adintuitive.com>
2025-05-08 15:22:26 -0400

    OT filter

---
+++ Package/Package.Infrastructure.AspNetCore/ActivityProcessors/FilterActivityProcessor2.cs
@@ -0,0 +1,18 @@
+﻿using OpenTelemetry;
+using System.Diagnostics;
+
+namespace Package.Infrastructure.AspNetCore.ActivityProcessors;
+public class FilterActivityProcessor2(Predicate<Activity> filter) : BaseProcessor<Activity>
+{
+    private readonly Predicate<Activity> _filter = filter ?? throw new ArgumentNullException(nameof(filter));
+
+    public override void OnEnd(Activity activity)
+    {
+        if (_filter(activity))
+        {
+            // Suppress the activity from being exported
+            activity.SetStatus(ActivityStatusCode.Unset); // Correct way to call SetStatus
+            activity.ActivityTraceFlags &= ~ActivityTraceFlags.Recorded;
+        }
+    }
+}
