81bdd1df70c0d51ce56882f4a684a00411279285
Eben Freeman <eben.freeman@adintuitive.com>
2025-06-28 14:49:39 -0400

    IRequestContext

--- Package/Package.Infrastructure.Common.Contracts/IRequestContext.cs
+++ Package/Package.Infrastructure.Common.Contracts/IRequestContext.cs
@@ -5,6 +5,7 @@ public interface IRequestContext<out TAuditIdType, out TTenantIdType>
     string CorrelationId { get; }
     TAuditIdType AuditId { get; }
     TTenantIdType? TenantId { get; } // Nullable to allow for no tenant context
-
     List<string> Roles { get; } // List of roles associated with the request context
+
+    bool RoleExists(string roleName);
 }
