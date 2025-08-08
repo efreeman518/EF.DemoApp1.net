e10373d2bdd2a478d4dd9da8c7e345865943f5cc
Eben Freeman <eben.freeman@adintuitive.com>
2025-08-08 11:18:34 -0400

    nugets

--- Package/Package.Infrastructure.MSGraph/Models/GraphUserRequest.cs
+++ Package/Package.Infrastructure.MSGraph/Models/GraphUserRequest.cs
@@ -8,6 +8,6 @@
 /// <param name="DisplayName"></param>
 /// <param name="Email"></param>
 /// <param name="ForceChangePasswordNextSignIn"></param>
-/// <param name="Password">Used only on Create; ignored on update</param>
+/// <param name="Password">Must be specified for CreateUser; ignored on update</param>
 /// <param name="AdditionalData">must conform to existing/custom defined AADB2C User Attributes</param>
 public record GraphUserRequest(string? Id, bool AccountEnabled, string DisplayName, string Email, bool ForceChangePasswordNextSignIn, string? Password = null, Dictionary<string, object>? AdditionalData = null);
--- Test.Integration/Application/TodoServiceTests.cs
+++ Test.Integration/Application/TodoServiceTests.cs
@@ -6,7 +6,6 @@ using Domain.Shared.Enums;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Package.Infrastructure.BackgroundServices;
 using Package.Infrastructure.Common.Extensions;
 using Test.Support;
