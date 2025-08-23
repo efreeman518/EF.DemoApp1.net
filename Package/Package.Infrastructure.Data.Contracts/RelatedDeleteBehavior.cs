bcf07ffdc1f6c61bd0bbc710c077befb135e9933
Eben Freeman <eben.freeman@adintuitive.com>
2025-08-23 13:22:06 -0400

    RelatedDeleteBehavior

---
+++ Package/Package.Infrastructure.Data.Contracts/RelatedDeleteBehavior.cs
@@ -0,0 +1,11 @@
+﻿namespace Package.Infrastructure.Data.Contracts;
+
+/// <summary>
+/// Indicates how to handle related entity deletions.
+/// </summary>
+public enum RelatedDeleteBehavior
+{
+    None = 0, // Do not remove any related entities
+    RelationshipOnly = 1, // Remove immediate relationship only, not any m-m related entities themselves
+    RelationshipAndEntity = 2 // Remove both immediate relationship and the m-m related entities themselves
+}
