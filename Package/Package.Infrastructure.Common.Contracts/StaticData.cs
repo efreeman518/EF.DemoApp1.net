913c2a4343a49e483daa5a7dcc376d2e6990f390
Eben Freeman <eben.freeman@adintuitive.com>
2025-09-03 21:31:42 -0400

    StaticData

--- Package/Package.Infrastructure.Common.Contracts/StaticData.cs
+++ Package/Package.Infrastructure.Common.Contracts/StaticData.cs
@@ -22,14 +22,14 @@ public record StaticData
     /// The value is stored as 'object' to accommodate any 'StaticList<T>' type.
     /// The property is get-only to prevent reassignment while allowing JSON population and internal mutation.
     /// </summary>
-    public Dictionary<string, object> Lists { get; set; } = [];
+    public Dictionary<string, object> Lists { get; init; } = [];
 
     /// <summary>
     /// Stores individual static items, keyed by a unique name.
     /// The value is stored as 'object' to accommodate any value type 'TValue'.
     /// The property is get-only to prevent reassignment while allowing JSON population and internal mutation.
     /// </summary>
-    public Dictionary<string, object> Values { get; set; } = [];
+    public Dictionary<string, object> Values { get; init; } = [];
 
     /// <summary>
     /// Adds a 'StaticList<T>' to the 'Lists' dictionary with a specified key.
